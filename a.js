a0d0efc3045d90114819aa695a8f6af76e0d4fe5
Sushmitha bujjireddy <sushmithareddy014@gmail.com>
2024-01-10 17:10:31 +0530

    react-2

---
+++ a.js
@@ -0,0 +1,8 @@
+ 
+
+ let x=100;
+ let y=200;
+ console.log(x+y)
+ //export default x
+ export let str1="react";
+ export let str2="learning";
---
+++ b.js
@@ -0,0 +1,3 @@
+import {str1,str2} from "./a.js"
+
+console.log(str1,str2)
